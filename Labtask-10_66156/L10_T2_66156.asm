2a69ea01c75b3e843163bfb58f68f48cf8fccd6b
manadeemcs <manadeem.cs@gmail.com>
2025-11-04 01:04:10 +0500

    Delete Labtask-10_66156 directory

--- Labtask-10_66156/L10_T2_66156.asm
+++
@@ -1,8 +0,0 @@
-org 100h
-.data
-a db 01000001b
-.code
-mov ax,@data
-mov ds,ax
-mov bl,a
-or  bl, 00100000b
--- Labtask-10_66156/L10_T3_66156.asm
+++
@@ -1,9 +0,0 @@
-org 100h
-.data
-a db 01100001b      
-
-.code
-mov ax, @data
-mov ds, ax
-mov bl, a
-and bl, 11011111b   
